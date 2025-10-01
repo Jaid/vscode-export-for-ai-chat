3e6b48e7776a1b2bb142e12045b56d1d187e8bfa
Jaid <6216144+Jaid@users.noreply.github.com>
2025-10-01 04:11:30 +0100

    Migrated to Bun bundler

--- x/build.ts
+++ x/build.ts
@@ -1,9 +1,52 @@
+import type {BunPlugin} from 'bun'
+import type {PackageJson} from 'type-fest'
+
 import * as path from 'forward-slash-path'
+import * as lodash from 'lodash-es'
 
 const rootFolder = path.resolve(import.meta.dirname, '..')
+const packageJsonPath = path.join(rootFolder, 'package.json')
+const packageJson = await Bun.file(packageJsonPath).json() as PackageJson
+const emitPackageJsonPlugin: BunPlugin = {
+  name: 'EmitPackageJsonPlugin',
+  setup(build) {
+    build.onEnd(async result => {
+      if (!result.success) {
+        return
+      }
+      const relevantPackage = lodash.pick(packageJson, [
+        'type',
+        'repository',
+        'homepage',
+        'version',
+        'description',
+        'engines',
+        'categories',
+        'displayName',
+        'contributes',
+        'activationEvents',
+        'extensionKind',
+      ])
+      const id = packageJson.name!.replace(/^vscode-/, '')
+      const outPackage = {
+        ...relevantPackage,
+        name: id,
+        main: 'index.js',
+        publisher: 'jaidchen',
+      }
+      const outPath = path.join(rootFolder, 'out', 'bun', 'package.json')
+      await Bun.write(outPath, JSON.stringify(outPackage))
+    })
+  },
+}
 await Bun.build({
-  entrypoints: [path.resolve(rootFolder, 'src', 'extension.ts')],
-  outdir: path.resolve(rootFolder, 'out', 'bun'),
+  entrypoints: [path.join(rootFolder, 'src', 'extension.ts')],
+  outdir: path.join(rootFolder, 'out', 'bun'),
   target: 'node',
+  minify: true,
   external: ['vscode'],
+  naming: {
+    entry: '[dir]/index.js',
+  },
+  plugins: [emitPackageJsonPlugin],
 })
