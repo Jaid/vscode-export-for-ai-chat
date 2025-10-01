ee95b30dce58169099b08e7763919e8417e9bad7
Jaid <6216144+Jaid@users.noreply.github.com>
2025-10-01 02:04:13 +0100

    Changed launcher
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/command/copyFolder.ts
+++ src/command/copyFolder.ts
@@ -1,6 +1,6 @@
 import * as path from 'path'
 
-import fs from 'fs-extra'
+import * as fs from 'fs-extra'
 import * as vscode from 'vscode'
 
 import {getChatPromptFromText} from 'src/getChatPrompt.js'
