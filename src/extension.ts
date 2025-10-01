ec1856fa54592c2662c23a580121e6a98b345dd3
Jaid <6216144+Jaid@users.noreply.github.com>
2025-10-01 17:07:22 +0100

    Added more logging
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,15 @@
 import * as vscode from 'vscode'
 
-import {copyCode} from './command/copyCode.js'
-import {copyFile} from './command/copyFile.js'
-import {copyFolder} from './command/copyFolder.js'
+import {copyCode} from 'src/command/copyCode.js'
+import {copyFile} from 'src/command/copyFile.js'
+import {copyFolder} from 'src/command/copyFolder.js'
+import {outputChannel} from 'src/outputChannel.js'
 
 export const activate = (context: vscode.ExtensionContext) => {
+  outputChannel.appendLine('Starting Export for AI Chat extension activation.')
   const copyCodeDisposable = vscode.commands.registerCommand('export-for-ai-chat.copyCode', copyCode)
   const copyFileDisposable = vscode.commands.registerCommand('export-for-ai-chat.copyFile', copyFile)
   const copyFolderDisposable = vscode.commands.registerCommand('export-for-ai-chat.copyFolder', copyFolder)
   context.subscriptions.push(copyCodeDisposable, copyFileDisposable, copyFolderDisposable)
+  outputChannel.appendLine('Export for AI Chat extension activated.')
 }
