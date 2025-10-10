67700655125eb639abff0a1576ad77aa7b38033c
Jaid <6216144+Jaid@users.noreply.github.com>
2025-10-10 08:50:46 +0100

    Fixed syncTemplate
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- x/syncTemplate.ts
+++ x/syncTemplate.ts
@@ -12,9 +12,9 @@ const updateContributes = async () => {
   const contributes = Bun.YAML.parse(await Bun.file(contributesFile).text()) as PackageJson['contributes']
   const templateContentSquashed = templateContent.replaceAll('\n', '').trim()
   // @ts-expect-error TS2769
-  lodash.set(contributes, 'configuration.properties["export-for-ai-chat.template"].default', templateContentSquashed)
+  lodash.set(contributes, 'configuration.properties.template.default', templateContentSquashed)
   const output = toYaml(contributes)
-  await Bun.write(contributesFile, `yaml-language-server: $schema=https://gist.githubusercontent.com/Jaid/1196746806dc7be1eaa7783e4d7b388a/raw/extension_manifest.schema.json#properties/contributes\n${output}`)
+  await Bun.write(contributesFile, `# yaml-language-server: $schema=https://gist.githubusercontent.com/Jaid/1196746806dc7be1eaa7783e4d7b388a/raw/extension_manifest.schema.json#properties/contributes\n${output}`)
 }
 const updateReadme = async () => {
   const defaultTextPattern = /(?<before>.+Current default.+?```.*?\n)(?<match>.+?)(?<after>\n```.+)/s
